08d2fd7c9cb0c46931ac48977105f1fe87c635a9
Kumar Abhinav <97421185+0xabhinav@users.noreply.github.com>
2025-01-15 23:58:18 +0530

    Disable wallet creation

--- src/config.js
+++ src/config.js
@@ -1,24 +1,24 @@
 import logo from '../public/logo.png';
 import { base } from "viem/chains";
 
-const getConfig = ({emailSupported}) => {
+const getConfig = ({ emailSupported }) => {
     const loginMethodsAndOrder = emailSupported ? {
-        "primary": ["email","farcaster"],
+        "primary": ["email", "farcaster"],
         // "overflow": ["farcaster"],
     } : {
         "primary": ["farcaster"],
     };
     return {
-    "defaultChain": base,
-    "supportedChains": [base],
-    "appearance": {
-        "accentColor": "#b4f6a6",
-        "theme": "#222224",
-        "showWalletLoginFirst": false,
-        "logo": logo
-    },
-    "loginMethodsAndOrder": loginMethodsAndOrder,
-    "embeddedWallets": { "createOnLogin": "all-users", "requireUserPasswordOnCreate": false },
+        "defaultChain": base,
+        "supportedChains": [base],
+        "appearance": {
+            "accentColor": "#b4f6a6",
+            "theme": "#222224",
+            "showWalletLoginFirst": false,
+            "logo": logo
+        },
+        "loginMethodsAndOrder": loginMethodsAndOrder,
+        "embeddedWallets": { "createOnLogin": "off", "requireUserPasswordOnCreate": false },
         "mfa": { "noPromptOnMfaRequired": false }
     }
 };
